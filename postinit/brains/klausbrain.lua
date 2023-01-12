b28af1c33b0cc993689bdc0028a528a090c74c04
Kivi <12345677890@qq.com>
2023-01-12 18:35:43 +0800

    修改Klaus的brain

--- postinit/brains/klausbrain.lua
+++ postinit/brains/klausbrain.lua
@@ -1,13 +1,15 @@
 local AddBrainPostInit = AddBrainPostInit
 GLOBAL.setfenv(1, GLOBAL)
 
-local RESET_COMBAT_DELAY = 10
-
 local function postinit(self)
 
     table.remove(self.bt.root.children, 1)
 
     table.remove(self.bt.root.children, 2)
+
+    local Attack = ChaseAndAttack(self.inst)
+    table.remove(self.bt.root.children, 3)
+    table.insert(self.bt.root.children, 3, Attack)
 
     table.remove(self.bt.root.children, 4)
 
